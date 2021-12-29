b0d9bd1aae84f7f34157617cda8621c725180f9a
Khailas R <khailas303@gmail.com>
2021-12-29 18:09:12 +0530

    socket connected

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const port = 3000 || process.env.PORT;
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 
-const app = express();
-const server = http.createServer();
+const expressApp = express();
+const port = 3000 || process.env.PORT;
+const server = http.createServer(expressApp);
+const socketIo = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+expressApp.use(express.static(path.join(__dirname, 'public')));     // static folder
 
-app.listen(port, () => {
+
+socketIo.on('connection', socket => {
+    console.log('Client connected')
+});    // runs when client connects
+
+
+server.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
